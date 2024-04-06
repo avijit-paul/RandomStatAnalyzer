561303457e1257b4de10b657b485a9decb145169
Avijit Paul <36258657+avijit-paul@users.noreply.github.com>
2024-04-07 00:40:35 +0200

    Add files via upload

---
+++ StatExcelAnalyze.py
@@ -0,0 +1,22 @@
+# define function
+def calculate_stat(excel_file, sheet_number, column_name):
+    df = pd.read_excel(excel_file, sheet_name=sheet_number)
+
+    column_data = df[column_name]
+
+    average = column_data.mean()
+    print("Average is :", average)
+    standard_deviation = column_data.std()
+    print("Standard deviation is :", standard_deviation)
+    variance = column_data.var()
+    print("Variance is :", variance)
+
+    return average, standard_deviation, variance
+
+#enter data
+excel_file = "excel2.xlsx"
+sheet_number = "Sheet1"
+column_name = "RVs"
+
+# call the function
+calculate_stat(excel_file, sheet_number, column_name)
